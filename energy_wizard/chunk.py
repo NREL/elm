41deb8396938baaf79b7817da0940f5a0e81216a
grantbuster <grant.buster@nrel.gov>
2023-08-15 14:52:51 -0600

    added chunking class to be used outside of embedding

---
+++ energy_wizard/chunk.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+Utility to break text up into overlapping chunks.
+"""
+
+from energy_wizard.abs import ApiBase
+
+
+class Chunker(ApiBase):
+    """
+    Class to break text up into overlapping chunks
+
+    NOTE: this will currently fail if a single paragraph is greater than
+    the token limit. Need to add logic to split large paragraphs.
+    """
+
+    def __init__(self, text, tag=None, tokens_per_chunk=500, overlap=1,
+                 token_limit=8191):
+        """
+        Parameters
+        ----------
+        text : str
+            Single body of text to break up. Works well if this is a single
+            document with empty lines between paragraphs.
+        tag : None | str
+            Optional reference tag to include at the beginning of each text
+            chunk
+        tokens_per_chunk : float
+            Nominal token count per text chunk
+        overlap : int
+            Number of paragraphs to overlap between chunks
+        token_limit : float
+            Hard limit on the maximum number of tokens that can be embedded at
+            once
+        """
+
+        super().__init__()
+
+        self._idc = 0  # iter index for chunk
+        self.text = text
+        self.tag = tag
+        self.tokens_per_chunk = tokens_per_chunk
+        self.overlap = overlap
+        self.token_limit = token_limit
+        self._chunks = self.chunk_text()
+
+    def __iter__(self):
+        self._idc = 0
+        return self
+
+    def __next__(self):
+        """Iterator returns one of the text chunks at a time
+
+        Returns
+        -------
+        str
+        """
+
+        if self._idc >= len(self):
+            raise StopIteration
+
+        out = self.chunks[self._idc]
+        self._idc += 1
+        return out
+
+    def __len__(self):
+        """Number of text chunks
+
+        Return
+        ------
+        int
+        """
+        return len(self.chunks)
+
+    @property
+    def chunks(self):
+        """List of overlapping text chunks (strings).
+
+        Returns
+        -------
+        list
+        """
+        return self._chunks
+
+    @property
+    def paragraphs(self):
+        paragraphs = self.text.split('\n\n')
+        paragraphs = [p for p in paragraphs if self.is_good_paragraph(p)]
+        return paragraphs
+
+    @staticmethod
+    def is_good_paragraph(paragraph):
+        """Basic tests to make sure the paragraph is useful text."""
+        if '.....' in paragraph:
+            return False
+        elif paragraph.strip().isnumeric():
+            return False
+        else:
+            return True
+
+    def chunk_text(self):
+        """Perform the text chunking operation
+
+        Returns
+        -------
+        chunks : list
+            List of strings where each string is an overlapping chunk of text
+        """
+
+        tokens = [self.count_tokens(p) for p in self.paragraphs]
+
+        chunks = []
+        current = [0]
+        tcount = 0
+
+        for i, token in enumerate(tokens):
+            tcount += token
+            if tcount < self.tokens_per_chunk:
+                current.append(i)
+            else:
+                tcount = 0
+
+                if len(current) > 0:
+                    chunks.append(current)
+                    current = []
+
+                if i > 0:
+                    current = [i]
+
+        if len(current) > 0:
+            chunks.append(current)
+
+        text_chunks = []
+        for chunk in chunks:
+            current_text_chunk = [self.paragraphs[i] for i in chunk]
+            current_text_chunk = '\n\n'.join(current_text_chunk)
+            if chunk[-1] < len(self.paragraphs) - self.overlap:
+                for k in range(1, self.overlap + 1):
+                    overlap_text = self.paragraphs[chunk[-1] + k]
+                    new = current_text_chunk + '\n\n' + overlap_text
+                    if self.count_tokens(new) < self.token_limit:
+                        current_text_chunk = new
+
+            text_chunks.append(current_text_chunk)
+
+        if self.tag is not None:
+            text_chunks = [self.tag + '\n\n' + chunk for chunk in text_chunks]
+
+        return text_chunks
--- energy_wizard/embed.py
+++ energy_wizard/embed.py
@@ -8,6 +8,7 @@ import os
 import logging
 
 from energy_wizard.abs import ApiBase
+from energy_wizard.chunk import Chunker
 
 
 logger = logging.getLogger(__name__)
@@ -19,7 +20,8 @@ class ChunkAndEmbed(ApiBase):
     DEFAULT_MODEL = 'text-embedding-ada-002'
     """Default model to do embeddings."""
 
-    def __init__(self, text, tag=None, model=None):
+    def __init__(self, text, tag=None, model=None, tokens_per_chunk=500,
+                 overlap=1, token_limit=8191):
         """
         Parameters
         ----------
@@ -37,7 +39,6 @@ class ChunkAndEmbed(ApiBase):
 
         self.text = text
         self.tag = tag
-        self.text_chunks = None
 
         if os.path.isfile(text):
             logger.info('Loading text file: {}'.format(text))
@@ -46,9 +47,10 @@ class ChunkAndEmbed(ApiBase):
 
         assert isinstance(self.text, str)
         self.text = self.clean_tables(self.text)
-        paragraphs = self.text.split('\n\n')
-        paragraphs = [p for p in paragraphs if self.is_good_paragraph(p)]
-        self.paragraphs = paragraphs
+
+        self.text_chunks = Chunker(self.text, tag=tag,
+                                   tokens_per_chunk=tokens_per_chunk,
+                                   overlap=overlap, token_limit=token_limit)
 
     @staticmethod
     def clean_tables(text):
@@ -75,81 +77,6 @@ class ChunkAndEmbed(ApiBase):
 
         return '\n'.join(lines)
 
-    @staticmethod
-    def is_good_paragraph(paragraph):
-        """Basic tests to make sure the paragraph is useful text."""
-        if '.....' in paragraph:
-            return False
-        elif paragraph.strip().isnumeric():
-            return False
-        else:
-            return True
-
-    def chunk_text(self, tokens_per_chunk=500, overlap=1, token_limit=8191):
-        """Chunk a large text string into multiple small chunks with overlap
-
-        NOTE: this will currently fail if a single paragraph is greater than
-        the token limit. Need to add a piece to split large paragraphs.
-
-        Parameters
-        ----------
-        tokens_per_chunk : float
-            Nominal token count per text chunk
-        overlap : int
-            Number of paragraphs to overlap between chunks
-        token_limit : float
-            Hard limit on the maximum number of tokens that can be embedded at
-            once
-
-        Returns
-        -------
-        text_chunks : list
-            List of strings where each string is an overlapping chunk of text
-        """
-
-        tokens = [self.count_tokens(p) for p in self.paragraphs]
-
-        chunks = []
-        current = [0]
-        tcount = 0
-
-        for i, token in enumerate(tokens):
-            tcount += token
-            if tcount < tokens_per_chunk:
-                current.append(i)
-            else:
-                tcount = 0
-
-                if len(current) > 0:
-                    chunks.append(current)
-                    current = []
-
-                if i > 0:
-                    current = [i]
-
-        if len(current) > 0:
-            chunks.append(current)
-
-        text_chunks = []
-        for chunk in chunks:
-            current_text_chunk = [self.paragraphs[i] for i in chunk]
-            current_text_chunk = '\n\n'.join(current_text_chunk)
-            if chunk[-1] < len(self.paragraphs) - overlap:
-                for k in range(1, overlap + 1):
-                    overlap_text = self.paragraphs[chunk[-1] + k]
-                    new = current_text_chunk + '\n\n' + overlap_text
-                    if self.count_tokens(new) < token_limit:
-                        current_text_chunk = new
-
-            text_chunks.append(current_text_chunk)
-
-        if self.tag is not None:
-            text_chunks = [self.tag + '\n\n' + chunk for chunk in text_chunks]
-
-        self.text_chunks = text_chunks
-
-        return text_chunks
-
     async def run_async(self, rate_limit=175e3):
         """Run text embedding
 
@@ -170,8 +97,9 @@ class ChunkAndEmbed(ApiBase):
             List of 1D arrays representing the embeddings for all text chunks
         """
 
-        if not isinstance(self.text_chunks, (list, tuple)):
-            msg = ('You must chunk the text before running async embeddings!')
+        if not isinstance(self.text_chunks, Chunker):
+            msg = ('You must init a Chunker obj with the text before '
+                   'running async embeddings!')
             logger.error(msg)
             raise RuntimeError(msg)
 
