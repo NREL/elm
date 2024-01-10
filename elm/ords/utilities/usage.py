ec4ae71a0b78d8cdef9b20569b0c76a99705b2df
ppinchuk <ppinchuk@nrel.gov>
2024-01-10 12:52:36 -0700

    Add retry decorators

--- elm/ords/utilities/usage.py
+++ elm/ords/utilities/usage.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 """ELM Ordinances usage tracking utilities."""
 import time
+import random
+import asyncio
+import logging
 from collections import deque
-from functools import total_ordering
+from contextlib import contextmanager
+from functools import total_ordering, wraps
+
+import openai
 
 from elm.base import ApiBase
+from elm.ords.utilities.exceptions import ELMOrdsRuntimeError
+
+
+logger = logging.getLogger(__name__)
 
 
 def count_openai_tokens(messages, model):
@@ -118,3 +128,157 @@ class UsageTracker:
                 self._total -= self._q.popleft().value
         except IndexError:
             pass
+
+
+def retry_with_exponential_backoff(
+    base_delay=1,
+    exponential_base=4,
+    jitter=True,
+    max_retries=3,
+    errors=(openai.RateLimitError, openai.APITimeoutError),
+):
+    """Retry a synchronous function with exponential backoff.
+
+    This decorator works out-of-the-box for OpenAI chat completions
+    calls. To configure it for other functions, set the `errors` input
+    accordingly.
+
+    Parameters
+    ----------
+    base_delay : int, optional
+        The base delay time, in seconds. This time will be multiplied by
+        the exponential_base (plus any jitter) during each retry
+        iteration. The multiplication applies *at the first retry*.
+        Therefore, if your base delay is ``1`` and your
+        `exponential_base` is ``4`` (with no jitter), the delay before
+        the first retry will be ``1 * 4 = 4`` seconds. The subsequent
+        delay will be ``4 * 4 = 16`` seconds, and so on.
+        By default, ``1``.
+    exponential_base : int, optional
+        The multiplication factor applied to the base `delay` input.
+        See description of `delay` for an example. By default, ``4``.
+    jitter : bool, optional
+        Option to include a random fractional adder (0 - 1) to the
+        `exponential_base` before multiplying by the `delay`. This can
+        help ensure each function call is submitted slightly offset from
+        other calls in a batch and therefore help avoid repeated rate
+        limit failures by a batch of submissions arriving simultaneously
+        to a service. By default, ``True``.
+    max_retries : int, optional
+        Max number of retries before raising an `ELMOrdsRuntimeError`.
+        By default, ``3``.
+    errors : tuple, optional
+        The error class(es) to signal a retry. Other errors will be
+        propagated without retrying.
+        By default, ``(openai.RateLimitError, openai.APITimeoutError)``.
+
+    References
+    ----------
+    https://github.com/openai/openai-cookbook/blob/main/examples/How_to_handle_rate_limits.ipynb
+    https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            num_retries = 0
+            delay = base_delay
+
+            while True:
+                try:
+                    return func(*args, **kwargs)
+                except errors as e:
+                    num_retries = _handle_retries(num_retries, max_retries, e)
+                    delay = _compute_delay(delay, exponential_base, jitter)
+                    logger.debug(
+                        f"Error: {e}.\nRetrying in {delay:.2f} seconds."
+                    )
+                    time.sleep(delay)
+
+        return wrapper
+
+    return decorator
+
+
+def async_retry_with_exponential_backoff(
+    base_delay=1,
+    exponential_base=4,
+    jitter=True,
+    max_retries=3,
+    errors=(openai.RateLimitError, openai.APITimeoutError),
+):
+    """Retry an asynchronous function with exponential backoff.
+
+    This decorator works out-of-the-box for OpenAI chat completions
+    calls. To configure it for other functions, set the `errors` input
+    accordingly.
+
+    Parameters
+    ----------
+    base_delay : int, optional
+        The base delay time, in seconds. This time will be multiplied by
+        the exponential_base (plus any jitter) during each retry
+        iteration. The multiplication applies *at the first retry*.
+        Therefore, if your base delay is ``1`` and your
+        `exponential_base` is ``4`` (with no jitter), the delay before
+        the first retry will be ``1 * 4 = 4`` seconds. The subsequent
+        delay will be ``4 * 4 = 16`` seconds, and so on.
+        By default, ``1``.
+    exponential_base : int, optional
+        The multiplication factor applied to the base `delay` input.
+        See description of `delay` for an example. By default, ``4``.
+    jitter : bool, optional
+        Option to include a random fractional adder (0 - 1) to the
+        `exponential_base` before multiplying by the `delay`. This can
+        help ensure each function call is submitted slightly offset from
+        other calls in a batch and therefore help avoid repeated rate
+        limit failures by a batch of submissions arriving simultaneously
+        to a service. By default, ``True``.
+    max_retries : int, optional
+        Max number of retries before raising an `ELMOrdsRuntimeError`.
+        By default, ``3``.
+    errors : tuple, optional
+        The error class(es) to signal a retry. Other errors will be
+        propagated without retrying.
+        By default, ``(openai.RateLimitError, openai.APITimeoutError)``.
+
+    References
+    ----------
+    https://github.com/openai/openai-cookbook/blob/main/examples/How_to_handle_rate_limits.ipynb
+    https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
+    """
+
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(*args, **kwargs):
+            num_retries = 0
+            delay = base_delay
+
+            while True:
+                try:
+                    return await func(*args, **kwargs)
+                except errors as e:
+                    num_retries = _handle_retries(num_retries, max_retries, e)
+                    delay = _compute_delay(delay, exponential_base, jitter)
+                    logger.debug(
+                        f"Error: {e}.\nRetrying in {delay:.2f} seconds."
+                    )
+                    await asyncio.sleep(delay)
+
+        return wrapper
+
+    return decorator
+
+
+def _handle_retries(num_retries, max_retries, error):
+    """Raise error if retry attempts exceed max limit"""
+    num_retries += 1
+    if num_retries > max_retries:
+        msg = f"Maximum number of retries ({max_retries}) exceeded"
+        raise ELMOrdsRuntimeError(msg) from error
+    return num_retries
+
+
+def _compute_delay(delay, exponential_base, jitter):
+    """Compute the next delay time"""
+    return delay * exponential_base * (1 + jitter * random.random())
--- tests/ords/utilities/test_utilities_usage.py
+++ tests/ords/utilities/test_utilities_usage.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Test ELM Ordinance usage utilities"""
 import time
+import random
 import asyncio
 from pathlib import Path
 
@@ -10,7 +11,10 @@ from elm.ords.utilities.usage import (
     count_openai_tokens,
     TimedEntry,
     UsageTracker,
+    retry_with_exponential_backoff,
+    async_retry_with_exponential_backoff,
 )
+from elm.ords.utilities.exceptions import ELMOrdsRuntimeError
 
 
 TEST_MESSAGES_1 = [
@@ -65,6 +69,45 @@ def test_usage_tracker():
     assert tracker.total == 200
     time.sleep(3)
     assert tracker.total == 0
+
+
+@pytest.mark.parametrize("jitter,bounds", [(False, (2, 3)), (True, (4, 5))])
+def test_sync_retry(jitter, bounds, monkeypatch):
+    """Test the `retry_with_exponential_backoff` decorator"""
+
+    monkeypatch.setattr(random, "random", lambda: 1, raising=True)
+
+    @retry_with_exponential_backoff(
+        exponential_base=2, max_retries=1, jitter=jitter, errors=(ValueError,)
+    )
+    def failing_function():
+        raise ValueError("I'm broken")
+
+    start_time = time.time()
+    with pytest.raises(ELMOrdsRuntimeError):
+        failing_function()
+    elapsed_time = time.time() - start_time
+    assert bounds[0] <= elapsed_time < bounds[1]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("jitter,bounds", [(False, (2, 3)), (True, (4, 5))])
+async def test_async_retry(jitter, bounds, monkeypatch):
+    """Test the `async_retry_with_exponential_backoff` decorator"""
+
+    monkeypatch.setattr(random, "random", lambda: 1, raising=True)
+
+    @async_retry_with_exponential_backoff(
+        exponential_base=2, max_retries=1, jitter=jitter, errors=(ValueError,)
+    )
+    async def failing_function():
+        raise ValueError("I'm broken")
+
+    start_time = time.time()
+    with pytest.raises(ELMOrdsRuntimeError):
+        await failing_function()
+    elapsed_time = time.time() - start_time
+    assert bounds[0] <= elapsed_time < bounds[1]
 
 
 if __name__ == "__main__":
