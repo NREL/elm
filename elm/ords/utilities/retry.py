e1ea0b284a668a009570e5e379208602427b2df1
ppinchuk <ppinchuk@nrel.gov>
2024-01-11 13:39:17 -0700

    Rename utilities module

--- elm/ords/utilities/retry.py
+++ elm/ords/utilities/retry.py
@@ -4,130 +4,14 @@ import time
 import random
 import asyncio
 import logging
-from collections import deque
-from contextlib import contextmanager
-from functools import total_ordering, wraps
+from functools import wraps
 
 import openai
 
-from elm.base import ApiBase
 from elm.ords.utilities.exceptions import ELMOrdsRuntimeError
 
 
 logger = logging.getLogger(__name__)
-
-
-def count_openai_tokens(messages, model):
-    """Count the number of tokens in an outgoing set of messages.
-
-    Parameters
-    ----------
-    messages : list
-        A list of message objects, where the latter is represented
-        using a dictionary. Each message dictionary must have a
-        "content" key containing the string to count tokens for.
-    model : str
-        The OpenAI model being used. This input will be passed to
-        :func:`tiktoken.encoding_for_model`.
-
-    Returns
-    -------
-    int
-        Total number of tokens in the set of messages outgoing to
-        OpenAI.
-
-    References
-    ----------
-    https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
-    """
-    message_total = sum(
-        ApiBase.count_tokens(message["content"], model=model) + 4
-        for message in messages
-    )
-    return message_total + 3
-
-
-@total_ordering
-class TimedEntry:
-    """An entry that performs comparisons based on time added, not value.
-
-    Examples
-    --------
-    >>> a = TimedEntry(100)
-    >>> a > 1000
-    True
-    """
-
-    def __init__(self, value):
-        """
-
-        Parameters
-        ----------
-        value : obj
-            Some value to store as an entry.
-        """
-        self.value = value
-        self._time = time.time()
-
-    def __eq__(self, other):
-        return self._time == other
-
-    def __lt__(self, other):
-        return self._time < other
-
-
-class TimeBoundedUsageTracker:
-    """Track usage of a resource over time.
-
-    This class wraps a double-ended queue, and any inputs older than
-    a certain time are dropped. Those values are also subtracted from
-    the running total.
-
-    References
-    ----------
-    https://stackoverflow.com/questions/51485656/efficient-time-bound-queue-in-python
-    """
-
-    def __init__(self, max_seconds=65):
-        """
-
-        Parameters
-        ----------
-        max_seconds : int, optional
-            Maximum age in seconds of an element before it is dropped
-            from consideration. By default, ``65``.
-        """
-        self.max_seconds = max_seconds
-        self._total = 0
-        self._q = deque()
-
-    @property
-    def total(self):
-        """float: Total value of all entries younger than `max_seconds`"""
-        self._discard_old_values()
-        return self._total
-
-    def add(self, value):
-        """Add a value to track.
-
-        Parameters
-        ----------
-        value : int | float
-            A new value to add to the queue. It's total will be added to
-            the running total, and it will live for `max_seconds` before
-            being discarded.
-        """
-        self._q.append(TimedEntry(value))
-        self._total += value
-
-    def _discard_old_values(self):
-        """Discard 'old' values from the queue"""
-        cutoff_time = time.time() - self.max_seconds
-        try:
-            while self._q[0] < cutoff_time:
-                self._total -= self._q.popleft().value
-        except IndexError:
-            pass
 
 
 def retry_with_exponential_backoff(
--- tests/ords/utilities/test_utilities_retry.py
+++ tests/ords/utilities/test_utilities_retry.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Test ELM Ordinance usage utilities"""
+"""Test ELM Ordinance retry utilities"""
 import time
 import random
 import asyncio
@@ -7,68 +7,11 @@ from pathlib import Path
 
 import pytest
 
-from elm.ords.utilities.usage import (
-    count_openai_tokens,
-    TimedEntry,
-    TimeBoundedUsageTracker,
+from elm.ords.utilities.retry import (
     retry_with_exponential_backoff,
     async_retry_with_exponential_backoff,
 )
 from elm.ords.utilities.exceptions import ELMOrdsRuntimeError
-
-
-TEST_MESSAGES_1 = [
-    {"role": "system", "content": "You are a friendly bot"},
-    {"role": "user", "content": "How are you?"},
-]
-TEST_MESSAGES_2 = [
-    {"role": "system", "content": "You are a friendly bot"},
-    {"role": "user", "content": "I have 5 apples."},
-    {"role": "system", "content": "Great!"},
-    {"role": "user", "content": "How many apples do you have?."},
-]
-
-
-@pytest.mark.parametrize(
-    "messages, model, token_count",
-    [(TEST_MESSAGES_1, "gpt-4", 20), (TEST_MESSAGES_2, "gpt-4", 39)],
-)
-def test_count_openai_tokens(messages, model, token_count):
-    """Test `count_openai_tokens` function"""
-    assert count_openai_tokens(messages, model) == token_count
-
-
-def test_timed_entry():
-    """Test `TimedEntry` class"""
-
-    a = TimedEntry(100)
-    assert a > 10000
-
-    time.sleep(1)
-    sample_time = time.time()
-    time.sleep(1)
-    b = TimedEntry(10000)
-    assert b > sample_time
-    assert a < sample_time
-
-    assert a.value == 100
-    assert b.value == 10000
-
-
-def test_time_bounded_usage_tracker():
-    """Test the `TimeBoundedUsageTracker` class"""
-
-    tracker = TimeBoundedUsageTracker(max_seconds=5)
-    assert tracker.total == 0
-    tracker.add(500)
-    assert tracker.total == 500
-    time.sleep(3)
-    tracker.add(200)
-    assert tracker.total == 700
-    time.sleep(3)
-    assert tracker.total == 200
-    time.sleep(3)
-    assert tracker.total == 0
 
 
 @pytest.mark.parametrize("jitter,bounds", [(False, (2, 3)), (True, (4, 5))])
