e4a7dc1809a1c243b623300b4cec28778a78d732
ppinchuk <ppinchuk@nrel.gov>
2024-01-31 13:29:24 -0700

    Add `format_html_tables` function

--- elm/utilities/parse.py
+++ elm/utilities/parse.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 """ELM parsing utilities."""
+import io
+import re
+import logging
+
 import numpy as np
+import pandas as pd
+
+
+logger = logging.getLogger(__name__)
 
 
 def is_double_col(text, separator="    "):
@@ -40,3 +48,59 @@ def remove_blank_pages(pages):
         List of strings with content, or empty list.
     """
     return [page for page in pages if any(page.strip())]
+
+
+def format_html_tables(text, **kwargs):
+    """Format tables within HTML text into pretty markdown.
+
+    Note that if pandas does not detect enough tables in the text to
+    match the "<table>" tags, no replacement is performed at all.
+
+    Parameters
+    ----------
+    text : str
+        HTML text, possible containing tables enclosed by the
+        "<table>" tag.
+    **kwargs
+        Keyword-arguments to pass to ``pandas.DataFrame.to_markdown``
+        function. Must not contain the `"headers"` keyword (this is
+        supplied internally).
+
+    Returns
+    -------
+    str
+        Text with HTML tables (if any) converted to markdown.
+    """
+    matches = _find_html_table_matches(text)
+    if not matches:
+        return text
+
+    dfs = _find_dfs(text)
+    if len(matches) != len(dfs):
+        logger.error(
+            "Found incompatible number of HTML (%d) and parsed (%d) tables! "
+            "No replacement performed.",
+            len(matches),
+            len(dfs),
+        )
+        return text
+
+    return _replace_tables_in_text(text, matches, dfs, **kwargs)
+
+
+def _find_html_table_matches(text):
+    """Find HTML table matches in the text"""
+    return re.findall("<table>[\s\S]*?</table>", text)
+
+
+def _find_dfs(text):
+    """Load HTML tables from text into DataFrames"""
+    return pd.read_html(io.StringIO(text))
+
+
+def _replace_tables_in_text(text, matches, dfs, **kwargs):
+    """Replace all items in the 'matches' input with MD tables"""
+    for table_str, df in zip(matches, dfs):
+        new_table_str = df.to_markdown(headers=df.columns, **kwargs)
+        text = text.replace(table_str, new_table_str)
+    return text
--- tests/utilities/test_utilities_parse.py
+++ tests/utilities/test_utilities_parse.py
@@ -4,7 +4,37 @@ from pathlib import Path
 
 import pytest
 
-from elm.utilities.parse import is_double_col, remove_blank_pages
+from elm.utilities.parse import (
+    is_double_col,
+    remove_blank_pages,
+    format_html_tables,
+)
+
+SAMPLE_TABLE_TEXT = """Some text.
+<table>
+    <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+    </tr>
+    <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+    </tr>
+    <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+    </tr>
+</table>
+"""
+EXPECTED_TABLE_OUT = """Some text.
+|    | Company                    | Contact         | Country   |
+|---:|:---------------------------|:----------------|:----------|
+|  0 | Alfreds Futterkiste        | Maria Anders    | Germany   |
+|  1 | Centro comercial Moctezuma | Francisco Chang | Mexico    |
+"""
 
 
 def test_is_double_col():
@@ -36,6 +66,14 @@ def test_remove_blank_pages():
     assert remove_blank_pages(["Here", ""]) == ["Here"]
     assert remove_blank_pages(["We", "  "]) == ["We"]
     assert remove_blank_pages(["", " Go ", "  ", "Again"]) == [" Go ", "Again"]
+
+
+def test_format_html_tables():
+    assert format_html_tables("test") == "test"
+    assert format_html_tables(SAMPLE_TABLE_TEXT) == EXPECTED_TABLE_OUT
+
+    bad_table_text = SAMPLE_TABLE_TEXT + "\nBad table:\n<table></table>"
+    assert format_html_tables(bad_table_text) == bad_table_text
 
 
 if __name__ == "__main__":
