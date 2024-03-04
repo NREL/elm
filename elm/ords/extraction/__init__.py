cb2255947a120fcc9efc812b0cab02f9808deba7
ppinchuk <ppinchuk@nrel.gov>
2024-03-04 12:17:06 -0700

    Add ngram check and retry for hallucinations

--- elm/ords/extraction/__init__.py
+++ elm/ords/extraction/__init__.py
@@ -1,5 +1,6 @@
 from .apply import (
     check_for_ordinance_info,
-    extract_ordinance_text,
+    extract_ordinance_text_with_llm,
+    extract_ordinance_text_with_ngram_validation,
     extract_ordinance_values,
 )
--- elm/ords/extraction/apply.py
+++ elm/ords/extraction/apply.py
@@ -26,7 +26,7 @@ async def check_for_ordinance_info(doc, text_splitter, **kwargs):
         ``"contains_ord_info"`` key, it will not be processed. To force
         a document to be processed by this function, remove that key
         from the documents metadata.
-    text_splitter : obj, optional
+    text_splitter : obj
         Instance of an object that implements a `split_text` method.
         The method should take text as input (str) and return a list
         of text chunks. Langchain's text splitters should work for this
@@ -63,8 +63,66 @@ async def check_for_ordinance_info(doc, text_splitter, **kwargs):
     return doc
 
 
-async def extract_ordinance_text(doc, text_splitter, **kwargs):
+async def extract_ordinance_text_with_llm(doc, text_splitter, extractor):
+    """Extract ordinance text from document using LLM.
+
+    Parameters
+    ----------
+    doc : elm.web.document.BaseDocument
+        A document known to contain ordinance information. This means it
+        must contain an ``"ordinance_text"`` key in the metadata. You
+        can run
+        :func:`~elm.ords.extraction.apply.check_for_ordinance_info`
+        to have this attribute populated automatically for documents
+        that are found to contain ordinance data. Note that if the
+        document's metadata does not contain the ``"ordinance_text"``
+        key, you will get an error.
+    text_splitter : obj
+        Instance of an object that implements a `split_text` method.
+        The method should take text as input (str) and return a list
+        of text chunks. Langchain's text splitters should work for this
+        input.
+    extractor : elm.ords.extraction.ordinance.OrdinanceExtractor
+        Instance of `~elm.ords.extraction.ordinance.OrdinanceExtractor`
+        used for ordinance text extraction.
+
+    Returns
+    -------
+    elm.web.document.BaseDocument
+        Document that has been parsed for ordinance text. The results of
+        the extraction are stored in the document's metadata. In
+        particular, the metadata will contain a
+        ``"cleaned_ordinance_text"`` key that will contain the cleaned
+        ordinance text.
+    """
+    text_chunks = text_splitter.split_text(doc.metadata["ordinance_text"])
+    ordinance_text = await extractor.check_for_restrictions(text_chunks)
+    doc.metadata["restrictions_ordinance_text"] = ordinance_text
+
+    text_chunks = text_splitter.split_text(ordinance_text)
+    ordinance_text = await extractor.check_for_correct_size(text_chunks)
+    doc.metadata["cleaned_ordinance_text"] = ordinance_text
+
+    return doc
+
+
+async def extract_ordinance_text_with_ngram_validation(
+    doc,
+    text_splitter,
+    n=4,
+    num_extraction_attempts=3,
+    ngram_fraction_threshold=0.95,
+    **kwargs,
+):
     """Extract ordinance text for a single document with known ord info.
+
+    This extraction includes an "ngram" check, which attempts to detect
+    wether or not the cleaned text was extracted from the original
+    ordinance text. The processing will attempt to re-extract the text
+    if the validation does not pass a certain threshold until the
+    maximum number of attempts is reached. If the text still does not
+    pass validation at this point, there is a good chance that the LLM
+    hallucinated parts of the output text, so caution should be taken.
 
     Parameters
     ----------
@@ -77,11 +135,25 @@ async def extract_ordinance_text(doc, text_splitter, **kwargs):
         that are found to contain ordinance data. Note that if the
         document's metadata does not contain the ``"ordinance_text"``
         key, it will not be processed.
-    text_splitter : obj, optional
+    text_splitter : obj
         Instance of an object that implements a `split_text` method.
         The method should take text as input (str) and return a list
         of text chunks. Langchain's text splitters should work for this
         input.
+    n : int, optional
+        Number of words to include per ngram for the ngram validation,
+        which helps ensure that the LLM did not hallucinate.
+        By default, ``4``.
+    num_extraction_attempts : int, optional
+        Number of extraction attempts before returning text that did not
+        pass the ngram check. If the processing exceeds this value,
+        there is a good chance that the LLM hallucinated parts of the
+        output text. Cannot be negative or 0. By default, ``3``.
+    ngram_fraction_threshold : float, optional
+        Fraction of ngrams in the cleaned text that are also found in
+        the original ordinance text for the extraction to be considered
+        successful. Should be a value between 0 and 1 (inclusive).
+        By default, ``0.95``.
     **kwargs
         Keyword-value pairs used to initialize an
         `elm.ords.llm.LLMCaller` instance.
@@ -108,14 +180,60 @@ async def extract_ordinance_text(doc, text_splitter, **kwargs):
     llm_caller = LLMCaller(**kwargs)
     extractor = OrdinanceExtractor(llm_caller)
 
-    text_chunks = text_splitter.split_text(doc.metadata["ordinance_text"])
-    ordinance_text = await extractor.check_for_restrictions(text_chunks)
-    doc.metadata["restrictions_ordinance_text"] = ordinance_text
+    doc = await _extract_with_ngram_check(
+        doc,
+        text_splitter,
+        extractor,
+        n=max(1, n),
+        num_tries=max(1, num_extraction_attempts),
+        ngram_fraction_threshold=max(0, min(1, ngram_fraction_threshold)),
+    )
 
-    text_chunks = text_splitter.split_text(ordinance_text)
-    ordinance_text = await extractor.check_for_correct_size(text_chunks)
-    doc.metadata["cleaned_ordinance_text"] = ordinance_text
+    return doc
 
+
+async def _extract_with_ngram_check(
+    doc,
+    text_splitter,
+    extractor,
+    n=4,
+    num_tries=3,
+    ngram_fraction_threshold=0.95,
+):
+    """Extract ordinance info from doc and validate using ngrams."""
+    from elm.ords.extraction.ngrams import sentence_ngram_containment
+
+    for attempt in range(num_tries):
+        doc = await extract_ordinance_text_with_llm(
+            doc, text_splitter, extractor
+        )
+        ngram_frac = sentence_ngram_containment(
+            original=doc.metadata["ordinance_text"],
+            test=doc.metadata["cleaned_ordinance_text"],
+            n=n,
+        )
+        if ngram_frac >= ngram_fraction_threshold:
+            logger.debug(
+                "Document extraction passed ngram check on attempt %d ",
+                "with score %.2f",
+                attempt + 1,
+                ngram_frac,
+            )
+            break
+        logger.debug(
+            "Document extraction failed ngram check on attempt %d "
+            "with score %.2f. Retrying...",
+            attempt + 1,
+            ngram_frac,
+        )
+    else:
+        msg = (
+            f"Ngram check failed after {num_tries}. LLM hallucination in "
+            "cleaned ordinance text is extremely likely! Proceed with "
+            "caution!!"
+        )
+        logger.warning(msg)
+        warn(msg, UserWarning)
     return doc
 
 
