b9321dea55ba4e58e46b776e15958965fa87d0cd
ppinchuk <ppinchuk@nrel.gov>
2024-03-08 09:17:26 -0700

    Add `load_counties_from_fp` function

--- elm/ords/utilities/__init__.py
+++ elm/ords/utilities/__init__.py
@@ -1,4 +1,4 @@
-from .counties import load_county_info
+from .counties import load_all_county_info, load_counties_from_fp
 from .parsing import llm_response_as_json, merge_overlapping_texts
 
 
--- tests/ords/utilities/test_utilities_counties.py
+++ tests/ords/utilities/test_utilities_counties.py
@@ -3,14 +3,20 @@
 from pathlib import Path
 
 import pytest
+import pandas as pd
 
-from elm.ords.utilities.counties import load_county_info, county_websites
+from elm.ords.utilities.counties import (
+    load_all_county_info,
+    load_counties_from_fp,
+    county_websites,
+)
+from elm.ords.utilities.exceptions import ELMOrdsValueError
 
 
 def test_load_counties():
-    """Test the `load_county_info` function."""
+    """Test the `load_all_county_info` function."""
 
-    county_info = load_county_info()
+    county_info = load_all_county_info()
     assert not county_info.empty
 
     expected_cols = [
@@ -35,7 +41,7 @@ def test_county_websites():
     """Test the `county_websites` function"""
 
     websites = county_websites()
-    assert len(websites) == len(load_county_info())
+    assert len(websites) == len(load_all_county_info())
     assert type(websites) == dict
     assert all(type(key) == tuple for key in websites)
     assert all(len(key) == 2 for key in websites)
@@ -44,6 +50,40 @@ def test_county_websites():
     assert ("decatur", "indiana") in websites
     assert ("el paso", "colorado") in websites
     assert ("box elder", "utah") in websites
+
+
+def test_load_counties_from_fp(tmp_path):
+    """Test `load_counties_from_fp` function."""
+
+    test_county_fp = tmp_path / "out.csv"
+    input_counties = pd.DataFrame(
+        {"County": ["decatur", "DNE County"], "State": ["INDIANA", "colorado"]}
+    )
+    input_counties.to_csv(test_county_fp)
+
+    counties = load_counties_from_fp(test_county_fp)
+
+    assert len(counties) == 1
+    assert set(counties["County"]) == {"Decatur"}
+    assert set(counties["State"]) == {"Indiana"}
+    assert {type(val) for val in counties["FIPS"]} == {int}
+
+
+def test_load_counties_from_fp_bad_input(tmp_path):
+    """Test `load_counties_from_fp` function."""
+
+    test_county_fp = tmp_path / "out.csv"
+    pd.DataFrame().to_csv(test_county_fp)
+
+    with pytest.raises(ELMOrdsValueError) as err:
+        load_counties_from_fp(test_county_fp)
+
+    expected_msg = (
+        "The following required columns were not found in the county " "input:"
+    )
+    assert expected_msg in str(err)
+    assert "County" in str(err)
+    assert "State" in str(err)
 
 
 if __name__ == "__main__":
