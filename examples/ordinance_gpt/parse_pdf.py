0ffbbbbd4957be5ab4dad0acf846417514b60765
grantbuster <grant.buster@nrel.gov>
2024-07-03 16:57:37 -0600

    Example is now running from pdf on disk

--- examples/ordinance_gpt/parse_pdf.py
+++ examples/ordinance_gpt/parse_pdf.py
@@ -1,8 +1,5 @@
-import threading
 import asyncio
 from elm.base import ApiBase
-from elm.pdf import PDFtoTXT
-from elm.chunk import Chunker
 from elm.web.document import PDFDocument
 from functools import partial
 import openai
@@ -11,33 +8,47 @@ from elm.ords.services.queues import initialize_service_queue
 from elm.ords.services.openai import OpenAIService
 from elm.ords.utilities import RTS_SEPARATORS
 from elm.ords.process import validate_api_params
-from elm.utilities.parse import read_pdf
+from elm.ords.llm import LLMCaller
+from elm.ords.extraction.ordinance import OrdinanceExtractor
+from elm.ords.extraction.apply import extract_ordinance_values
 from elm.ords.services.provider import RunningAsyncServices
 from rex import init_logger
 
-from elm.ords.extraction.apply import check_for_ordinance_info
+from elm.ords.extraction.apply import (check_for_ordinance_info,
+                                       extract_ordinance_text_with_llm)
 
 
-async def run(services, doc, text_splitter, **kwargs):
-    async with RunningAsyncServices(services):
-        doc = await check_for_ordinance_info(doc, text_splitter, **kwargs)
-    return doc
-
-
-if __name__ == '__main__':
-    init_logger(__name__, log_level='DEBUG')
-    init_logger('elm', log_level='DEBUG')
-
-    fp = './county_ord_files/Box Elder County, Utah.pdf'
-
+async def clean_text(services, doc, **kwargs):
     text_splitter = RecursiveCharacterTextSplitter(
         RTS_SEPARATORS,
         chunk_size=3000,
         chunk_overlap=300,
         length_function=partial(ApiBase.count_tokens, model='gpt-4'),
     )
+    async with RunningAsyncServices(services):
+        extractor = OrdinanceExtractor(LLMCaller(**kwargs))
+        doc = await check_for_ordinance_info(doc, text_splitter, **kwargs)
+        doc = await extract_ordinance_text_with_llm(doc, text_splitter,
+                                                    extractor)
+    return doc
 
-    doc = PDFDocument.from_file(fp)
+
+async def extract_ordinances(services, doc, **kwargs):
+    async with RunningAsyncServices(services):
+        doc = await extract_ordinance_values(doc, **kwargs)
+    return doc
+
+
+if __name__ == '__main__':
+    init_logger('elm', log_level='INFO')
+
+    fp_pdf = './examples/ordinance_gpt/county_ord_files/Box Elder County, Utah.pdf'
+
+    fp_txt_all = fp_pdf.replace('.pdf', '.txt')
+    fp_txt_clean = fp_pdf.replace('.pdf', '_clean.txt')
+    fp_ords = fp_pdf.replace('.pdf', '_ords.csv')
+
+    doc = PDFDocument.from_file(fp_pdf)
 
     azure_api_key, azure_version, azure_endpoint = validate_api_params()
     client = openai.AsyncAzureOpenAI(api_key=azure_api_key,
@@ -46,38 +57,13 @@ if __name__ == '__main__':
     llm_service = OpenAIService(client, rate_limit=1e9)
     initialize_service_queue(llm_service.__class__.__name__)
     services = [llm_service]
+    kwargs = dict(llm_service=llm_service, model='gpt-4', temperature=0)
 
-    kwargs = dict(llm_service=llm_service, model='gpt-4', temperature=0,
-                  max_tokens=1000)
+    doc = asyncio.run(clean_text(services, doc, **kwargs))
+    doc = asyncio.run(extract_ordinances(services, doc, **kwargs))
 
-    doc = asyncio.run(run(services, doc, text_splitter, **kwargs))
-
-    breakpoint()
-    raise
-    #doc = asyncio.run(check_for_ordinance_info(doc, text_splitter, **kwargs))
-
-    #print(doc.metadata["ordinance_text"])
-    #breakpoint()
-    #raise
-
-#    kwargs = dict(model="gpt-4",
-#                  usage_tracker=None,
-#                  usage_sub_label='document_content_validation',
-#                  messages=[
-#                      {"role": "system", "content": "You are a helpful assistant."},
-#                      {"role": "user", "content": "Hello!"}
-#                    ],
-#                  temperature=0,
-#                  max_tokens=1000)
-    #asyncio.run(llm_service(
-
-
-#    kwargs = dict(model="gpt-4",
-#                  messages=[
-#                      {"role": "system", "content": "You are a helpful assistant."},
-#                      {"role": "user", "content": "Hello!"}
-#                    ],
-#                  temperature=0,
-#                  max_tokens=1000)
-#    out = asyncio.run(llm_service._call_gpt(**kwargs))
-#    print(out)
+    doc.metadata['ordinance_values'].to_csv(fp_ords)
+    with open(fp_txt_all, 'w') as f:
+        f.write(doc.metadata["ordinance_text"])
+    with open(fp_txt_clean, 'w') as f:
+        f.write(doc.metadata["cleaned_ordinance_text"])
