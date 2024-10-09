8d795f55a2eac4ebb5442eb67d74f7360735a304
ppinchuk <pinchukpaul@gmail.com>
2024-10-09 14:15:50 -0600

    Docsting fixes

--- elm/ords/download.py
+++ elm/ords/download.py
@@ -150,14 +150,14 @@ async def download_county_ordinance(
 
     Parameters
     ----------
-    location : elm.ords.utilities.location.Location
+    location : :class:`elm.ords.utilities.location.Location`
         Location objects representing the county.
     text_splitter : obj, optional
         Instance of an object that implements a `split_text` method.
         The method should take text as input (str) and return a list
-        of text chunks. Raw text from HTML pages will be passed through 
-        this splitter to split the single wep page into multiple pages 
-        for the output document. Langchain's text splitters should work 
+        of text chunks. Raw text from HTML pages will be passed through
+        this splitter to split the single wep page into multiple pages
+        for the output document. Langchain's text splitters should work
         for this input.
     num_urls : int, optional
         Number of unique Google search result URL's to check for
