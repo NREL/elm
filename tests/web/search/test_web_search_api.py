6714f4e688da62608b351a8f6b979d5d5fb26352
ppinchuk <pinchukpaul@gmail.com>
2025-02-06 14:16:59 -0700

    Add tests for `APIDuckDuckGoSearch`

--- tests/web/search/test_web_search_api.py
+++ tests/web/search/test_web_search_api.py
@@ -4,7 +4,13 @@ from pathlib import Path
 
 import pytest
 
+import elm.web.search.duckduckgo
 from elm.web.search.base import APISearchEngineLinkSearch
+
+
+SE_API_TO_TEST = [(elm.web.search.duckduckgo.APIDuckDuckGoSearch, {}),
+                  (elm.web.search.duckduckgo.APIDuckDuckGoSearch,
+                   {"api_key": None})]
 
 
 def test_api_key_read_from_env(monkeypatch):
@@ -29,6 +35,25 @@ def test_no_api_key_var():
             return []
 
     assert MockAPISearchEngine().api_key is None
+
+
+@pytest.mark.parametrize("queries",[['1. "NREL elm"'],
+                                    ['1. "NREL elm"', "NREL reV"],])
+@pytest.mark.parametrize("se", SE_API_TO_TEST)
+@pytest.mark.asyncio
+async def test_basic_search_query(queries, se):
+    """Test basic web search query functionality"""
+
+    num_results = 7
+    se_class, kwargs = se
+    search_engine = se_class(**kwargs)
+    out = await search_engine.results(*queries, num_results=num_results)
+
+    assert len(out) == len(queries)
+    for results in out:
+        assert len(results) == num_results
+        assert all(link.startswith("http") for link in results)
+        assert all("+" not in link for link in results)
 
 
 if __name__ == "__main__":
