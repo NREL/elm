5e33920f31960731a2aac5e349976b61a0be5fa4
ppinchuk <pinchukpaul@gmail.com>
2025-02-06 11:52:12 -0700

    Refactor away a new base class

--- elm/web/search/base.py
+++ elm/web/search/base.py
@@ -16,8 +16,72 @@ from elm.web.utilities import clean_search_query, pw_page
 logger = logging.getLogger(__name__)
 
 
-class PlaywrightSearchEngineLinkSearch(ABC):
-    """Search for top results on a given search engine and return links"""
+class SearchEngineLinkSearch(ABC):
+    """Abstract class to retrieve links for a query using a search engine"""
+
+    _SE_NAME = "<unknown se>"
+    _EXCEPTION_TO_CATCH = Exception
+
+    async def results(self, *queries, num_results=10):
+        """Retrieve links for the first `num_results` of each query
+
+        This function executes a search for each input query and
+        returns a list of links corresponding to the top `num_results`.
+
+        Parameters
+        ----------
+        *queries : str
+            One or more queries to search for.
+        num_results : int, optional
+            Number of top results to retrieve for each query. Note that
+            this value can never exceed the number of results per page
+            (typically 10). If you pass in a larger value, it will be
+            reduced to the number of results per page.
+            By default, ``10``.
+
+        Returns
+        -------
+        list
+            List equal to the length of the input queries, where each
+            entry is another list containing the top `num_results`
+            links.
+        """
+        queries = map(clean_search_query, queries)
+        return await self._get_links(queries, num_results)
+
+    async def _get_links(self, queries, num_results):
+        """Get links for multiple queries"""
+        outer_task_name = asyncio.current_task().get_name()
+
+        searches = [
+            asyncio.create_task(
+                self._skip_exc_search(query, num_results=num_results),
+                name=outer_task_name,
+            )
+            for query in queries
+        ]
+        logger.trace("Kicking off %s search for %d queries",
+                     self._SE_NAME, len(searches))
+        results = await asyncio.gather(*searches)
+        logger.trace("Got results for link search:\n%r", results)
+        return results
+
+    async def _skip_exc_search(self, query, num_results=10):
+        """Perform search while ignoring errors"""
+        try:
+            return await self._search(query, num_results=num_results)
+        except self._EXCEPTION_TO_CATCH as e:
+            logger.exception(e)
+            return []
+
+    @abstractmethod
+    async def _search(self, query, num_results=10):
+        """Search web for links related to a query"""
+        raise NotImplementedError
+
+
+class PlaywrightSearchEngineLinkSearch(SearchEngineLinkSearch):
+    """Abstract class to search the web using playwright and a search engine"""
 
     MAX_RESULTS_CONSIDERED_PER_PAGE = 10
     """Number of results considered per search engine page"""
@@ -25,8 +89,8 @@ class PlaywrightSearchEngineLinkSearch(ABC):
     PAGE_LOAD_TIMEOUT = 90_000
     """Default page load timeout value in milliseconds"""
 
-    _SE_NAME = "<unknown se>"
     _SC = StealthConfig(navigator_user_agent=False)
+    _EXCEPTION_TO_CATCH = PlaywrightTimeoutError
 
     def __init__(self, **launch_kwargs):
         """
@@ -67,14 +131,6 @@ class PlaywrightSearchEngineLinkSearch(ABC):
             logger.trace("Extracting links for query: %r", query)
             return await self._extract_links(page, num_results)
 
-    async def _skip_exc_search(self, query, num_results=10):
-        """Perform search while ignoring timeout errors"""
-        try:
-            return await self._search(query, num_results=num_results)
-        except PlaywrightTimeoutError as e:
-            logger.exception(e)
-            return []
-
     async def _get_links(self, queries, num_results):
         """Get links for multiple queries"""
         outer_task_name = asyncio.current_task().get_name()
@@ -99,31 +155,6 @@ class PlaywrightSearchEngineLinkSearch(ABC):
         links = await asyncio.to_thread(page.locator, self._SE_SR_TAG)
         return [await links.nth(i).get_attribute("href")
                 for i in range(num_results)]
-
-    async def results(self, *queries, num_results=10):
-        """Retrieve links for the first `num_results` of each query.
-
-        This function executes a google search for each input query and
-        returns a list of links corresponding to the top `num_results`.
-
-        Parameters
-        ----------
-        num_results : int, optional
-            Number of top results to retrieve for each query. Note that
-            this value can never exceed the number of results per page
-            (typically 10). If you pass in a larger value, it will be
-            reduced to the number of results per page.
-            By default, ``10``.
-
-        Returns
-        -------
-        list
-            List equal to the length of the input queries, where each
-            entry is another list containing the top `num_results`
-            links.
-        """
-        queries = map(clean_search_query, queries)
-        return await self._get_links(queries, num_results)
 
     @property
     @abstractmethod
