05f335baf79a6f7d49cc4213495db01f209b36ed
ppinchuk <pinchukpaul@gmail.com>
2025-02-06 23:40:16 -0700

    Add missing exceptions

--- elm/exceptions.py
+++ elm/exceptions.py
@@ -6,5 +6,13 @@ class ELMError(Exception):
     """Generic ELM Error."""
 
 
+class ELMKeyError(ELMError, KeyError):
+    """ELM Key Error."""
+
+
+class ELMInputError(ELMError, ValueError):
+    """ELM Input (Value) Error."""
+
+
 class ELMRuntimeError(ELMError, RuntimeError):
     """ELM RuntimeError."""
