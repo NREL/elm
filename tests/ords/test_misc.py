0afa2bb721443e8df1206123d708c88988c4fa99
ppinchuk <pinchukpaul@gmail.com>
2025-07-02 15:47:32 -0600

    Fix date + tests

--- tests/ords/test_misc.py
+++ tests/ords/test_misc.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Miscellaneous ELM Ordinance tests"""
 from pathlib import Path
+from datetime import datetime
 
 import pytest
 
@@ -31,6 +32,48 @@ def test_parse_date_multiple_elements_year_biggest():
     dates = [{"year": 2023, "month": 7, "day": 15},
              {"year": 2024, "month": 7, "day": 15}]
     assert _parse_date(dates) == (2024, 7, 15)
+
+
+def test_parse_date_bad_element():
+    """Test `_parse_date` with only one component"""
+    dates = [{"year": 2023, "month": 10, "day": 15}]
+    assert _parse_date(dates) == (2023, 10, 15)
+
+
+def test_parse_date_one_bad_element():
+    """Test `_parse_date` with only one component"""
+    dates = [{"year": 2023, "month": 20, "day": 15}]
+    assert _parse_date(dates) == (2023, None, 15)
+
+    dates = [{"year": 1900, "month": 7, "day": 15}]
+    assert _parse_date(dates) == (None, 7, 15)
+
+    dates = [{"year": datetime.now().year + 1, "month": 7, "day": 15}]
+    assert _parse_date(dates) == (None, 7, 15)
+
+    dates = [{"year": 2023, "month": 7, "day": 32}]
+    assert _parse_date(dates) == (2023, 7, None)
+
+
+def test_parse_date_one_component():
+    """Test `_parse_date` with only one component"""
+    dates = [{"year": 2023}]
+    assert _parse_date(dates) == (2023, None, None)
+
+
+def test_parse_date_multiple_components_with_missing_elements():
+    """Test `_parse_date` with multiple components with missing elements"""
+    dates = [{"year": None, "month": 7, "day": 15},
+             {"year": 2024, "month": None, "day": None}]
+    assert _parse_date(dates) == (2024, None, None)
+
+    dates = [{"year": 2024, "month": 7, "day": 15},
+             {"year": 2024, "month": None, "day": 30}]
+    assert _parse_date(dates) == (2024, 7, 15)
+
+    dates = [{"year": 2024, "month": None, "day": 15},
+             {"year": 2024, "month": None, "day": 30}]
+    assert _parse_date(dates) == (2024, None, 30)
 
 
 if __name__ == "__main__":
